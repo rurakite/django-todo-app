54424b0b03031a0241df1aeb535504412343cd48
rurakite <ruravictor@gmail.com>
2023-05-21 15:01:23 +0000

    Add quotes to allowed hosts link

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1lfoxgp-wlx&yn+k85std0#1iz2^djkujnr&*9^)4_70#2^v9t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [my-django-todo-app.herokuapp.com]
+ALLOWED_HOSTS = ['my-django-todo-app.herokuapp.com']
 
 
 # Application definition
